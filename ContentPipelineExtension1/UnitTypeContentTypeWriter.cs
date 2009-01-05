ae109cda404db07f11d1df3d02e22097fdbe9f37
SND\tscheffel_cp <SND\tscheffel_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2009-01-05 02:34:39 +0000

    git-svn-id: https://xnapanzer.svn.codeplex.com/svn@21414 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ ContentPipelineExtension1/UnitTypeContentTypeWriter.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+using XmlContentShared;
+
+namespace ContentPipelineExtension
+{
+    /// <summary>
+    /// This class will be instantiated by the XNA Framework Content Pipeline
+    /// to write the specified data type into binary .xnb format.
+    ///
+    /// This should be part of a Content Pipeline Extension Library project.
+    /// </summary>
+    [ContentTypeWriter]
+    public class UnitTypeContentTypeWriter : ContentTypeWriter<XmlContentShared.UnitType>
+    {
+        protected override void Write(ContentWriter _output, XmlContentShared.UnitType _value)
+        {
+            _output.Write(_value.AirAttack);
+            _output.Write(_value.AirDefense);
+            _output.Write(_value.Ammo);
+            _output.WriteObject<DateTime>(_value.AvailabilityEnd);
+            _output.WriteObject<DateTime>(_value.AvailabilityStart);
+            _output.Write(_value.Characteristics);
+            _output.Write(_value.CloseDefense);
+            _output.Write(_value.CombatRange);
+            _output.Write(_value.Cost);
+            _output.Write(_value.EntrenchmentRate);
+            _output.Write(_value.Fuel);
+            _output.Write(_value.GroundDefense);
+            _output.Write(_value.HardAttack);
+            _output.Write(_value.ID);
+            _output.Write(_value.Initiative);
+            _output.WriteObject<XmlContentShared.GroundMovementClass>(_value.MovementClass);
+            _output.Write(_value.Moves);
+            _output.Write(_value.Name);
+            _output.Write(_value.Nationality);
+            _output.Write(_value.SoftAttack);
+            _output.Write(_value.SpottingRange);
+            _output.Write(_value.SpritesheetX);
+            _output.Write(_value.SpritesheetY);
+        }
+
+        public override string GetRuntimeReader(TargetPlatform targetPlatform)
+        {
+            return typeof(XmlContentShared.UnitTypeContentTypeReader).AssemblyQualifiedName;
+        }
+    }
+}
